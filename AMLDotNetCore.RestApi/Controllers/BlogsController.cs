f92fb66f5206664e8c2e00876966192081347110
AyeMyatLwin <108206521+ayemyatlwin@users.noreply.github.com>
2024-09-27 23:34:23 +0630

    feat: asp.net core web api (read => get all data )

---
+++ AMLDotNetCore.RestApi/Controllers/BlogsController.cs
@@ -0,0 +1,45 @@
+﻿using AMLDotNetCore.DataBase.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMLDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly AppDbContext _db= new AppDbContext();
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            var lst = _db.TblBlogs.AsNoTracking().ToList();
+            //var lst = _db.TblBlogs.AsNoTracking().ToList().Where(x => x.DeleteFlag == false);
+            return Ok(new { message = "success",resqData=lst });
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog()
+        {
+            return Ok();
+        }
+
+        [HttpPatch]
+        public IActionResult PatchBlog()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult UpdateBlog()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteBlog()
+        {
+            return Ok();
+        }
+    }
+}
