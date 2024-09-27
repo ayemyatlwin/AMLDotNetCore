9dd4ee812178c666bbf53d5e79e8a15428b3ee2b
AyeMyatLwin <108206521+ayemyatlwin@users.noreply.github.com>
2024-09-27 22:22:11 +0630

    feat : connect with class library

---
+++ AMLDotNetCore.ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AMLDotNetCore.DataBase.Models;
+
+Console.WriteLine("Hello, World!");
+
+AppDbContext db= new AppDbContext();
+var lst = db.TblBlogs.ToList();
+foreach (var item in lst)
+{
+    Console.WriteLine(item.BlogId);
+    Console.WriteLine(item.BlogTitle);
+    Console.WriteLine(item.BlogAuthot);
+    Console.WriteLine(item.BlogContent);
+    Console.WriteLine("================");
+
+}
---
+++ AMLDotNetCore.DataBase/Models/TblBlog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMLDotNetCore.DataBase.Models;
+
+public partial class TblBlog
+{
+    public int BlogId { get; set; }
+
+    public string BlogTitle { get; set; } = null!;
+
+    public string BlogAuthot { get; set; } = null!;
+
+    public string BlogContent { get; set; } = null!;
+
+    public bool DeleteFlag { get; set; }
+}
