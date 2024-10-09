b72ba14209df156c3c0010c7b73f84f60f78e8a0
AyeMyatLwin <108206521+ayemyatlwin@users.noreply.github.com>
2024-10-09 09:12:18 +0630

    feat : asp .net core web api create with ado .net

---
+++ AMLDotNetCore.RestApi/Controllers/BlogsAdoDotNetController.cs
@@ -0,0 +1,52 @@
+﻿using AMLDotNetCore.DataBase.Models;
+using AMLDotNetCore.RestApi.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace AMLDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsAdoDotNetController : ControllerBase
+    {
+        public readonly string _connectionString = "Data Source=.;Initial Catalog=BlogManagementDatabase;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            List<BlogViewModels> lst = new List<BlogViewModels>();
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            string query = @"SELECT [BlogId]
+                          ,[BlogTitle]
+                          ,[BlogAuthot]
+                          ,[BlogContent]
+                          ,[DeleteFlag]
+                      FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                //Console.WriteLine(reader["BlogId"]);
+                //Console.WriteLine(reader["BlogTitle"]);
+                //Console.WriteLine(reader["BlogAuthot"]);
+                //Console.WriteLine(reader["BlogContent"]);
+                lst.Add(new BlogViewModels
+                {
+                    Id = Convert.ToInt32(reader["BlogId"]),
+                    Title = Convert.ToString(reader["BlogTitle"]),
+                    Author = Convert.ToString(reader["BlogAuthot"]),
+                    Content = Convert.ToString(reader["BlogContent"]),
+                    DeleteFlag = Convert.ToBoolean(reader["DeleteFlag"])
+                });
+            }
+            connection.Close();
+            return Ok(lst);
+        }
+
+      
+    }
+}
