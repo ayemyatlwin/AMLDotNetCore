824644c1cde2095baef5ee97984123df2576800d
AyeMyatLwin <108206521+ayemyatlwin@users.noreply.github.com>
2024-10-12 19:44:55 +0630

    feat : modify Blog Vew Model

--- AMLDotNetCore.RestApi/ViewModels/BlogViewModels.cs
+++ AMLDotNetCore.RestApi/ViewModels/BlogViewModels.cs
@@ -3,11 +3,11 @@
     public class BlogViewModels
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
-        public string Content { get; set; }
+        public string? Content { get; set; }
 
-        public string Author { get; set; }
+        public string? Author { get; set; }
 
         public bool DeleteFlag { get; set; }
     }
