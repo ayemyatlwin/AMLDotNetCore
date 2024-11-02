8982cc32929907908b60f1b7da65c471850bdb91
AyeMyatLwin <108206521+ayemyatlwin@users.noreply.github.com>
2024-11-02 23:28:09 +0630

    feat : blog web api with dapper service

---
+++ AMLDotNetCore.RestApi/Controllers/BlogDapperController2.cs
@@ -0,0 +1,130 @@
+﻿using AMLDotNetCore.RestApi.DataModels;
+using AMLDotNetCore.RestApi.ViewModels;
+using AMLDotNetCore.Shared;
+using Dapper;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace AMLDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogDapperController2 : ControllerBase
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=BlogManagementDatabase;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
+        private readonly DapperService _dapperService;
+
+        public BlogDapperController2()
+        {
+            _dapperService = new DapperService(_connectionString);
+        }
+        private bool BlogExists(int id)
+        {
+                string query = @"SELECT * FROM tbl_blog WHERE DeleteFlag = 0 AND BlogId = @BlogId";
+                var blog = _dapperService.Query<BlogDataModel>(query, new { BlogId = id }).FirstOrDefault();
+                return blog != null;
+        }
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            string query = "select * from tbl_blog where DeleteFlag = 0;";
+            List<BlogDataModel> lst = _dapperService.Query<BlogDataModel>(query).ToList();
+            return Ok(lst);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBlogById(int id)
+        {
+            if (!BlogExists(id))
+            {
+                return NotFound("Blog not found.");
+            }
+
+            string query = @"select * from tbl_blog where DeleteFlag = 0 and BlogId = @BlogId";
+
+            var item = _dapperService.Query<BlogDataModel>(query, new BlogDataModel
+            {
+                BlogId = id
+            }).FirstOrDefault();
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+            return Ok(item);
+
+
+
+        }
+
+        [HttpPost]
+        public IActionResult CreateBlog(BlogDataModel blog)
+        {
+            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+                    ([BlogTitle]
+                    ,[BlogAuthot]
+                    ,[BlogContent]
+                    ,[DeleteFlag])
+            VALUES
+                    (@BlogTitle
+                    ,@BlogAuthot
+                    ,@BlogContent
+                    ,0)";
+            
+                int result = _dapperService.Execute(query, new BlogDataModel
+                {
+                    BlogTitle = blog.BlogTitle,
+                    BlogAuthot = blog.BlogAuthot,
+                    BlogContent = blog.BlogContent
+                });
+                return Ok(result == 1 ? "Saving Successful " : "Saving failed");
+            
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateBlog(int id, BlogDataModel blog)
+        {
+            if (!BlogExists(id))
+            {
+                return NotFound("Blog not found.");
+            }
+
+            string query = $@"UPDATE [dbo].[Tbl_Blog]
+                    SET [BlogTitle] = @BlogTitle
+                       ,[BlogAuthot] = @BlogAuthot
+                       ,[BlogContent] = @BlogContent
+                       ,[DeleteFlag] = 0
+                  WHERE BlogId = @BlogId";
+
+            int result = _dapperService.Execute(query, new BlogDataModel
+            {
+                BlogId = id,
+                BlogTitle = blog.BlogTitle!,
+                BlogAuthot = blog.BlogAuthot!,
+                BlogContent = blog.BlogContent!,
+            });
+            return Ok(result == 1 ? "Updating Successful." : "Updating Fail");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBlog(int id)
+        {
+
+            if (!BlogExists(id))
+            {
+                return NotFound("Blog not found.");
+            }
+
+            string query = $@"UPDATE [dbo].[Tbl_Blog]
+                            SET [DeleteFlag] = 1
+                            WHERE BlogId = @BlogId";
+            int result = _dapperService.Execute(query, new BlogDataModel { BlogId = id });
+
+            return Ok(result == 0 ? "Deleting Blog Failed !" : "Successfully Deleted Blog");
+
+        }
+    }
+}
