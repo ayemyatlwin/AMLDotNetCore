e334dca070af2a2f33446f5b40f2f1ae4ec3ae6a
AyeMyatLwin <108206521+ayemyatlwin@users.noreply.github.com>
2024-12-03 10:38:55 +0630

    feat : http client readasync

---
+++ AMLDotNetCore.ConsoleApp3/HttpClientExample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMLDotNetCore.ConsoleApp3
+{
+    public class HttpClientExample
+    {
+        private readonly HttpClient client;
+        private readonly string postEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
+        public HttpClientExample()
+        {
+            client = new HttpClient();
+        }
+
+        public async Task ReadAsync()
+        {
+            var response = await client.GetAsync(postEndpoint);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonStr);
+            }
+        }
+    }
+}
---
+++ AMLDotNetCore.ConsoleApp3/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AMLDotNetCore.ConsoleApp3;
+
+Console.WriteLine("Hello, World!");
+
+HttpClientExample example = new HttpClientExample();
+await example.ReadAsync();
